f8dd7eb6cc90eb9bf45131174388d8c8f4f7aa90
Yosvaa <yosvany.c.a@gmail.com>
2025-02-14 20:41:43 +0100

    prueba2

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-import firebase from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js";
-// Required for side-effects
-import "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-    FIREBASE_CONFIGURATION
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-
-
-// Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-
-let body = document.querySelector("body");
-body.appendChild(document.createElement("hr"));
+export { db, collection, addDoc, getDocs, doc, deleteDoc, getDoc, updateDoc };
